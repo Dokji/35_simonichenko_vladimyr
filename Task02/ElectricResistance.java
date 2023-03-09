a70f9fc74967b4054df2c24607cc6e6e04519865
Dokji <114224307+Dokji@users.noreply.github.com>
2023-03-09 11:35:30 +0200

    Add files via upload

---
+++ Task02/ElectricResistance.java
@@ -0,0 +1,42 @@
+package Task02;
+
+
+import java.io.Serializable;
+
+public class ElectricResistance implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final double voltage1;
+    private final double voltage2;
+    private final double voltage3;
+    private final double current;
+    private final double resistance;
+    
+    public ElectricResistance(double v1, double v2, double v3, double i) {
+        voltage1 = v1;
+        voltage2 = v2;
+        voltage3 = v3;
+        current = i;
+        resistance = (voltage1 + voltage2 + voltage3) / current;
+    }
+    
+    public double getResistance() {
+        return resistance;
+    }
+    public double calculateTotalResistance() { 
+
+        double totalVoltage = voltage1 + voltage2 + voltage3;
+        return totalVoltage / current;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getHexadecimalRepresentation() { 
+
+        double resistance = calculateTotalResistance();
+        int hexValue = (int) resistance;
+        return Integer.toHexString(hexValue);
+    }
+}
+
